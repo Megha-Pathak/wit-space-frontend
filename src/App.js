0bf63132c034d9a395295de5850bf49ea9f2cbe3
smilegupta <guptamiley3012@gmail.com>
2021-07-15 16:03:16 +0530

    📦 NEW: Code cleaning and Added CRUD folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Home from "components/Home";
-import Signup from "components/Signup";
-import Login from "components/Login";
-import Sidebar from "components/Sidebar";
-import Showcase from "components/Showcase";
-import Discussions from "components/Discussions";
-import DevSelfCare from "components/DevSelfCare";
-import Opportunities from "components/Opportunities";
-import PeerProgramming from "components/PeerProgramming";
-import Resources from "components/Resources";
 import MiniDrawer from "components/MiniDrawer";
 
 export default function App() {
   return (
-   <MiniDrawer/>
+    <button> Login </button>
+    
+  //  <MiniDrawer/>
   );
 }
